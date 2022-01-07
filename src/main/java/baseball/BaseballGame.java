d4e58a65619c441b602a2b6414342978862d215d
hochan222 <hochan049@gmail.com>
2022-01-07 16:47:32 +0900

    feat: 정답 맞는지 확인하고, 결과를 출력한다.

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -8,6 +8,9 @@ public class BaseballGame {
 
     StringBuilder _computerBallCount = new StringBuilder();
     StringBuilder _userBallCount = new StringBuilder();
+
+    int _strikeCount = 0;
+    int _ballCount = 0;
     int _retryCount = 0;
 
     public int getRandomSingleDigit() {
@@ -48,12 +51,15 @@ public class BaseballGame {
     }
 
     public void printComputerBallCount() {
-        System.out.println(_computerBallCount.toString());
+        System.out.println("computer number: " + _computerBallCount.toString());
     }
 
     public void play() {
         setComputerNumber();
+        printComputerBallCount();
         setUserBallCountInput();
+
+        checkAnswer();
 
         System.out.println(_userBallCount.toString());
         System.out.println(_state);
@@ -63,5 +69,51 @@ public class BaseballGame {
         while (_state == "play") {
             printComputerBallCount();
         }
+    }
+
+    public boolean checkAnswer() {
+        if (checkEqualToComputerBallCount()) {
+            System.out.println("세개의 숫자를 모두 맞히셨습니다. 게임 종료");
+            return true;
+        }
+        setStrikeAndBallCount();
+        if (_strikeCount > 0) {
+            System.out.printf(_strikeCount + "스트라이크");
+            if (_ballCount > 0) {
+                System.out.printf(" "+_ballCount + "볼");
+            }
+        }
+        if (_strikeCount == 0 && _ballCount > 0) {
+            System.out.printf(_ballCount + "볼");
+        }
+        return false;
+    }
+
+    public boolean checkEqualToComputerBallCount() {
+        return _userBallCount.equals(_computerBallCount);
+    }
+
+    public void setStrikeAndBallCount() {
+        int strikeCount = 0;
+        int ballCount = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (checkStrike(_computerBallCount, _userBallCount, i)) {
+                strikeCount++;
+            } else if (checkBall(_computerBallCount, _userBallCount, i)) {
+                ballCount++;
+            }
+        }
+
+        _strikeCount = strikeCount;
+        _ballCount = ballCount;
+    }
+
+    public boolean checkStrike(StringBuilder ballCountA, StringBuilder ballCountB, int index) {
+        return ballCountA.charAt(index) == ballCountB.charAt(index);
+    }
+
+    public boolean checkBall(StringBuilder ballCountA, StringBuilder ballCountB, int index) {
+        return ballCountA.toString().contains(String.valueOf(ballCountB.charAt(index)));
     }
 }
